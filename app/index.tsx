679825b0d1c3591222d88b519ae479e1ac8fcb63
Alwahdi <abdullahalwahdi464@gmail.com>
2025-09-27 06:45:06 +0300

    initialize clerk authentication

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,37 @@
+import { SignOutButton } from "@/app/components/SignOutButton";
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
-import "./globals.css";
+import './globals.css';
 
-export default function App() {
+export default function Page() {
+  const { user } = useUser();
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'white'
+    }}>
+      <SignedIn>
+        <Text style={{
+          fontSize: 18,
+          fontWeight: 'bold',
+          marginBottom: 16
+        }}>
+          Hello {user?.emailAddresses[0].emailAddress}
+        </Text>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <Link href="/(auth)/sign-in" style={{ marginBottom: 8 }}>
+          <Text style={{ color: '#3B82F6', fontSize: 16 }}>Sign in</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text style={{ color: '#3B82F6', fontSize: 16 }}>Sign up</Text>
+        </Link>
+      </SignedOut>
     </View>
   );
 }
